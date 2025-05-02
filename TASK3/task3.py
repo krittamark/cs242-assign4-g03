b948b2a08d381f20bb5de401a10f913aeb6cb666
krittamark <33484828+krittamark@users.noreply.github.com>
2025-05-03 01:25:52 +0700

    feat: enhance web scraping to count tables and extract header sources

--- TASK3/task3.py
+++ TASK3/task3.py
@@ -14,3 +14,15 @@ else:
     print(f"Request to {url} failed !!")
     print(f"Status code: {response.status_code} -- {response.reason}")
     exit()
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+allTablesCount = len(soup.find_all('table'))
+print(f"The web page contains {allTablesCount} tables in total.")
+
+targetTable = soup.find('table', class_='wikitable')
+sourcesTableHead = targetTable.find('tr', class_='static-row-header').find_all('th')[1:]
+sources = [th.find('a').text for th in sourcesTableHead]
+
+print(f"We found {len(sources)} header rows indicating info sources as follows.")
+print(sources)
